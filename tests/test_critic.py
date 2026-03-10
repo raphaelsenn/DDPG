ec1576935c02231646c69de45668a626065f76fd
raphaelsenn <raphaelsenn@outlook.de>
2026-03-10 14:24:43 +0100

    fixed typo in test_critic

--- tests/test_critic.py
+++ tests/test_critic.py
@@ -7,7 +7,7 @@ from ddpg import CriticMLP
 
 
 @pytest.fixture
-def actor() -> CriticMLP:
+def critic() -> CriticMLP:
     return CriticMLP(784, 400, 300, 10)
 
 
@@ -41,23 +41,24 @@ def action_np() -> torch.Tensor:
     return np.random.randn(10).astype(np.float32)
 
 
-class TestActorMLP:
-    def test_dims(self, actor: CriticMLP) -> None:
-        assert actor.state_dim == 784
-        assert actor.h1_dim == 400
-        assert actor.h2_dim == 300
-        assert actor.action_dim == 10
+class TestCriticMLP:
+    def test_dims(self, critic: CriticMLP) -> None:
+        assert critic.state_dim == 784
+        assert critic.h1_dim == 400
+        assert critic.h2_dim == 300
+        assert critic.action_dim == 10
 
-    def test_forward_shape(self, actor: CriticMLP, state_batch: torch.Tensor, action_batch: torch.Tensor) -> None:
-        out = actor(state_batch, action_batch)
+    def test_forward_shape(self, critic: CriticMLP, state_batch: torch.Tensor, action_batch: torch.Tensor) -> None:
+        out = critic(state_batch, action_batch)
         assert out.shape == (64, 1)
+        assert isinstance(out, torch.Tensor)
 
-    def test_predict_numpy_1(self, actor: CriticMLP, state: torch.Tensor, action: np.ndarray) -> None:
-        out = actor.predict(state, action)
+    def test_predict_numpy_1(self, critic: CriticMLP, state: torch.Tensor, action: np.ndarray) -> None:
+        out = critic.predict(state, action)
         assert isinstance(out, np.ndarray)
         assert out.shape == (1, 1)
 
-    def test_predict_numpy_2(self, actor: CriticMLP, state_np: np.ndarray, action_np: np.ndarray) -> None:
-        out = actor.predict(state_np, action_np)
+    def test_predict_numpy_2(self, critic: CriticMLP, state_np: np.ndarray, action_np: np.ndarray) -> None:
+        out = critic.predict(state_np, action_np)
         assert isinstance(out, np.ndarray)
         assert out.shape == (1, 1)
